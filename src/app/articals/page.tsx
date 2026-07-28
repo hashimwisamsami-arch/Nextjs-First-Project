06f082e2ad086bce8f7b6fec11e27b793a47dfc8
Hashim <hashimwisamsami@email.com>
2026-07-28 12:50:02 +0300

    make article section with article type and ArticalItem.tsx.

--- src/app/articals/page.tsx
+++ src/app/articals/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import ArticalItem from "@/components/articals/ArticalItem";
 
-const Artical = () => {
-  return <div>Artical</div>;
+import { Articale } from "@/utils/types";
+
+const Artical = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const articles: Articale[] = await response.json();
+  return (
+    <section className="container m-auto px-5">
+      <div className="flex items-center flex-wrap justify-center gap-7">
+        {articles.map((item) => (
+          <ArticalItem arttical={item} key={item.id} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Artical;
---
+++ src/components/articals/ArticalItem.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import { Articale } from "@/utils/types";
+interface ArticalItemProps {
+  arttical: Articale;
+}
+
+const ArticalItem = ({ arttical }: ArticalItemProps) => {
+  return (
+    <div
+      className="
+    group
+    w-full sm:w-[45%] lg:w-[23%]
+    p-6
+    my-3
+    rounded-2xl
+    bg-white
+    shadow-md
+    border border-gray-200
+    hover:shadow-2xl
+    hover:-translate-y-2
+    transition-all
+    duration-300
+  "
+    >
+      <h3
+        className="
+      text-2xl
+      font-bold
+      text-gray-900
+      mb-4
+      group-hover:text-purple-700
+      transition
+    "
+      >
+        {arttical.title}
+      </h3>
+
+      <p
+        className="
+      text-gray-600
+      text-base
+      leading-7
+      mb-6
+      line-clamp-3
+    "
+      >
+        {arttical.body}
+      </p>
+
+      <Link
+        href={`/articles/${arttical.id}`}
+        className="
+      flex
+      items-center
+      justify-center
+      w-full
+      bg-linear-to-r
+      from-purple-600
+      to-indigo-600
+      hover:from-purple-700
+      hover:to-indigo-700
+      text-white
+      font-semibold
+      rounded-xl
+      py-3
+      shadow-md
+      hover:shadow-lg
+      transition-all
+      duration-300
+    "
+      >
+        Read More →
+      </Link>
+    </div>
+  );
+};
+
+export default ArticalItem;
