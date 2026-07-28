6418657c9ec557d8fe8557e4eb41f949cc08e9db
Hashim <hashimwisamsami@email.com>
2026-07-28 13:55:45 +0300

    craete not found page with footer.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -8,7 +8,7 @@ interface ErrorPageProps {
 
 const ErrorPage = ({ error, reset }: ErrorPageProps) => {
   return (
-    <div className="pt-7 text-center">
+    <div className="fix_height pt-7 text-center">
       <div className="text-3xl text-red-700 font-semibold">
         Something went wrong
       </div>
