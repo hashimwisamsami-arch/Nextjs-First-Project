c76e4b40699163a96e06435216619417d06cf6b7
Hashim <hashimwisamsami@email.com>
2026-08-12 14:52:01 +0300

    make search articles work with the api.

--- src/apiCalls/articleApiCall.ts
+++ src/apiCalls/articleApiCall.ts
@@ -22,3 +22,16 @@ export async function getArticlesCount(): Promise<number> {
   const { count } = (await response.json()) as { count: number };
   return count;
 }
+
+//get article base on search text
+export async function getArticleBasedOnSearch(
+  searchText: string,
+): Promise<Article[]> {
+  const response = await fetch(
+    `http://localhost:3000/api/articles/search?searchText=${searchText}`,
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch articles");
+  }
+  return response.json();
+}
--- src/app/articles/search/page.tsx
+++ src/app/articles/search/page.tsx
@@ -1,3 +1,7 @@
+import { getArticleBasedOnSearch } from "@/apiCalls/articleApiCall";
+import ArticalItem from "@/components/articals/ArticalItem";
+import { Article } from "@/generated/prisma/client";
+
 interface ArticleSearchPageProps {
   searchParams: Promise<{
     searchText?: string;
@@ -6,9 +10,29 @@ interface ArticleSearchPageProps {
 
 const ArticleSearchPage = async ({ searchParams }: ArticleSearchPageProps) => {
   const { searchText } = await searchParams;
+  const articles: Article[] = await getArticleBasedOnSearch(searchText ?? "");
   return (
     <section className="fix_height container m-auto px-5">
-      <h1 className="text-2xl font-bold">Search Text is: {searchText}</h1>
+      {articles.length === 0 ? (
+        <h2 className="text-gray-800 text-2xl font-bold p-5">
+          Articles based on{" "}
+          <span className="text-red-500 mx-1">{searchText}</span> not found
+        </h2>
+      ) : (
+        <>
+          <h1 className="text-2xl font-bold mb-2 mt-7 text-gray-700">
+            Article based on:
+            <span className="ms-1 text-green-700 text-3xl font-bold">
+              {searchText}
+            </span>
+          </h1>
+          <div className="flex items-center justify-center flex-wrap gap-7">
+            {articles.map((item) => (
+              <ArticalItem key={item.id} arttical={item} />
+            ))}
+          </div>
+        </>
+      )}
     </section>
   );
 };
